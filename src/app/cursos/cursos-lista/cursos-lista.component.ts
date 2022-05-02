430862e286e8c08940ee95c9175f3486a1f29b1e
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-05-02 11:46:32 -0300

    Http = Listando Registros

---
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styleUrls: ['./cursos-lista.component.scss']
+})
+export class CursosListaComponent implements OnInit {
+
+  cursos!: Curso[];
+  constructor(private service: CursosService) { }
+
+  ngOnInit(): void {
+    this.service.list().subscribe(dados => this.cursos = dados);
+  }
+
+}
